07c104b401545dbd92f298844d14c02146b95e87
Guilhereme Silva <guime.silva333@gmail.com>
2026-03-22 17:14:14 -0300

    feat: add CardProcessor and comments study

---
+++ src/main/java/br/com/nerdstore/pagamentos/introducao/CardProcessor.java
@@ -0,0 +1,19 @@
+package br.com.nerdstore.pagamentos.introducao;
+
+/**
+ * Process purchases with credit card
+ *
+ * @author Guilherme Silva
+ * @version 1.0
+ */
+public class CardProcessor {
+    public static void main(String[] args) {
+        // Due to the purchase value, approval is immediate.
+        System.out.println("Compra aprovada!");
+
+        /*
+            TODO: Add verification for high-value purchases.
+            TODO: Add SMS purchase alerts to the cardholder.
+         */
+    }
+}
